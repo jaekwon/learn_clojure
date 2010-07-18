2251f37b1bc854e1fe5fe93d8fc89a0abd159316
Jae Kwon <jae@flywheelnetworks.com>
2010-07-17 20:04:38 -0700

    Initial commit to learn_clojure.

---
+++ test/test_learn_clojure.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+class TestLearnClojure < Test::Unit::TestCase
+  should "probably rename this file and start testing for real" do
+    flunk "hey buddy, you should probably rename this file and start testing for real"
+  end
+end
